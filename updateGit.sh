9196dd42feee301733080177f61dc487acc90a45
Sergio Ruiz <serginator@gmail.com>
2011-04-24 21:09:42 +0200

    last update

---
+++ updateGit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd /home/serginator/Dropbox/Public/web/arkanoidTest/
+git add .
+git commit -m 'last update'
+git push origin master
+cd /home/serginator/Dropbox/Public/web/spaceInvaders/
+git add .
+git commit -m 'last update'
+git push origin master
+cd /home/serginator/Dropbox/Public/web/snake/
+git add .
+git commit -m 'last update'
+git push origin master
+

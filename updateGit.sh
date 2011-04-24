ee4c36c425cf6ecb5b9659f04dfc04d7086d9e55
Sergio Ruiz <serginator@gmail.com>
2011-04-24 21:32:42 +0200

    last update

--- updateGit.sh
+++ updateGit.sh
@@ -11,4 +11,7 @@ cd /home/serginator/Dropbox/Public/web/snake/
 git add .
 git commit -m 'last update'
 git push origin master
-
+cd /home/serginator/Dropbox/Public/web/serginator.github.com/
+git add .
+git commit -m 'last update'
+git push origin master
